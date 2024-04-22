f61389416847213fd6a080b0fb50f369a917894b
Parker Hutcheson <72584282+parkerhutcheson@users.noreply.github.com>
2024-04-22 14:21:49 +0900

    Removed body

--- api/assistants/create.ts
+++ api/assistants/create.ts
@@ -1,7 +1,7 @@
 const options = {
   method: 'POST',
   headers: {Authorization: 'Bearer 9f593270-7deb-4f44-8f5f-5fb27d315725', 'Content-Type': 'application/json'},
-  body: ``
+  // body: ``
 };
 
 fetch('https://api.vapi.ai/assistant', options)

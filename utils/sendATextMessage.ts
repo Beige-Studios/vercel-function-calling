be4784f4bfdd3f939d76bae0fcc8e12a23b0e8be
Parker Hutcheson <72584282+parkerhutcheson@users.noreply.github.com>
2024-04-22 18:35:23 +0900

    removed some comments

---
+++ utils/sendATextMessage.ts
@@ -0,0 +1,20 @@
+import twilio from 'twilio';
+
+const client: twilio.Twilio = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+
+async function sendATextMessage(phoneNumber: string, text: string): Promise<String> {
+    try {
+        const message = await client.messages.create({
+            from: '+12513334611',
+            to: phoneNumber,
+            body: text
+        });
+        console.log(message.sid);
+        return 'Success';
+    } catch (error) {
+        console.error('Error sending message:', error);
+        return 'Failed'
+    }
+}
+
+export default sendATextMessage;
